96be41b6715854ab924d9ca89cd0716b07cc1344
Dogu <dogudeniz.ugur@gmail.com>
2020-08-19 23:06:39 +0300

    Parsed the data from Medium and returned as json

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_stories_by_username():
     user_name = query_params['user_name']
 
     # Send the request to medium
-    target_url ='https://medium.com/@'+user_name+'?format=json'
+    target_url = 'https://medium.com/@' + user_name + '?format=json'
     r = requests.get(url=target_url)
 
     # Parse data
@@ -26,10 +26,9 @@ def get_stories_by_username():
     content = r.iter_lines()
     for element in content:
         data += str(element)
-    data = data.replace('])}while(1);</x>', '')
+    data = json.loads(data.replace('])}while(1);</x>', '')[2:-1].replace('\\', ''))
 
-    return json.dumps({'status': r.status_code, 'data': data})
-
+    return data
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
