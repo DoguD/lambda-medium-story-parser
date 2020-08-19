b96d2f09f025a81c23df12cd9921602a297f49d1
Dogu <dogudeniz.ugur@gmail.com>
2020-08-19 23:09:30 +0300

    comment cleanup

--- app.py
+++ app.py
@@ -29,23 +29,3 @@ def get_stories_by_username():
     data = json.loads(data.replace('])}while(1);</x>', '')[2:-1].replace('\\', ''))
 
     return data
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
